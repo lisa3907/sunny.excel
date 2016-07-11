d5bbedfbbca5f7afdd5ab99008d189892710c66a
odinsoft <odinsoft@odinsoftware.co.kr>
2016-07-11 16:55:05 +0900

    ClosedXml Source delete

--- src/ClosedXML/Excel/AutoFilters/IXLFilteredColumn.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-namespace ClosedXML.Excel
-{
-    public interface IXLFilteredColumn
-    {
-        IXLFilteredColumn AddFilter<T>(T value) where T : IComparable<T>;
-    }
-}
--- src/ClosedXML/Excel/RichText/IXLRichText.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ClosedXML.Excel
-{
-    public interface IXLRichText : IXLFormattedText<IXLRichText>
-    {
-    }
-}
